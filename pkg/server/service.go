75e5f2dfd567a50df53e02af59768c7b6d2a323a
Fabio Felipe Gutierrez <1560@holbertonschool.com>
2022-06-08 22:51:56 -0500

    Edit: erasing comments

--- pkg/server/service.go
+++ pkg/server/service.go
@@ -13,20 +13,6 @@ func (a *api) checkMutant(w http.ResponseWriter, r *http.Request) {
 	json.Unmarshal(body, &cdn)
 
 	mutantHandler(cdn.Dna, w, r)
-	//pp := strconv.FormatBool(isMutant(cdn.Dna))
-
-	//fmt.Fprintf(w, pp)
-	/*if !isMutant(cdn.Dna) {
-			w.WriteHeader(http.StatusForbidden)
-			fmt.Println(pp)
-			fmt.Fprintf(w, cdn.Dna[0])
-		} else {
-			//w.WriteHeader(http.StatusForbidden)
-			fmt.Fprintf(w, cdn.Dna[2])
-			//returnForbidden(w, r)
-		}
-
-	}*/
 }
 
 func mutantHandler(sample []string, w http.ResponseWriter, r *http.Request) {
@@ -38,9 +24,4 @@ func mutantHandler(sample []string, w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusForbidden)
 		fmt.Fprintf(w, "HTTP 403-FORBIDDEN")
 	}
-}
-
-func returnForbidden(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusForbidden)
-	w.Write([]byte("403 HTTP status code returned!"))
 }

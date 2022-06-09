7cad0c00bdb520eb7cb49172849d6ae71b1cfe37
Fabio Felipe Gutierrez <1560@holbertonschool.com>
2022-06-09 03:42:10 -0500

    Edit: editing service route

--- pkg/server/api.go
+++ pkg/server/api.go
@@ -18,9 +18,7 @@ func New() Server {
 	a := &api{}
 
 	r := mux.NewRouter()
-	//r.HandleFunc("/home", a.homeLink).Methods(http.MethodGet)
-	//r.HandleFunc("/event", a.checkMutant).Methods(http.MethodPost)
-	r.HandleFunc("/mutant", a.checkMutant).Methods(http.MethodPost)
+	r.HandleFunc("/mutant/", a.checkMutant).Methods(http.MethodPost)
 	a.router = r
 	return a
 }
